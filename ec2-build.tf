a067720fa76df4c2fe6f9e501dfd5180118af13e
engineeredwin <gtitechnet@gmail.com>
2022-12-13 19:50:20 -0600

    updated file

--- ec2-build.tf
+++ ec2-build.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-    
+  region  = "us-east-1"
+  profile = "cliaccess"
+}
+
+resource "aws_instance" "newserv" {
+  ami                    = "ami-0b0dcb5067f052a63"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = ["sg-0f1b059fdeab0b47a"]
+
+  tags = {
+    name    = "terra-instance"
+    project = "cliaccess-project"
+  }
 }
